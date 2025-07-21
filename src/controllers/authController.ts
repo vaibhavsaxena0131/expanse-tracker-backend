0722b772448a08ae87356b9e76e70767eca774de
Vaibhav <vaibhavsna175@gmail.com>
2025-07-21 12:23:10 +0530

    cookie issue

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -10,17 +10,15 @@ export const login = async (req: Request, res: Response, next: NextFunction) => 
     res.cookie('accessToken', accessToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
-      maxAge: 15 * 60 * 1000,
-      path: '/'
+      sameSite: 'none',
+      maxAge: 15 * 60 * 1000
     });
 
     res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-      path: '/'
+      sameSite: 'none',
+      maxAge: 7 * 24 * 60 * 60 * 1000
     });
 
     res.json({ user });
